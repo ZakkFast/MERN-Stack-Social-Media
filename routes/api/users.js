b94d03a69c9398b08566713c0a6cfc71610f3345
Zakk Fast <zakkfastpro@gmail.com>
2021-10-04 17:10:28 -0700

    set up auth for registar user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
-// @route    GET api/users
-// @decs     Test route
+// @route    POST api/users
+// @decs     Register user
 // @access   Public
-router.get("/", (req, res) => res.send("User Route"));
+router.post(
+  "/",
+  [
+    //Validation for name, email, and password
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() }); // Send bad request if error with json storin errors
+    }
+    res.send("User Route");
+  }
+);
 
 module.exports = router;
