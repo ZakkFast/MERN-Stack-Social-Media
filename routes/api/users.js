63aa89f61bd645f1565af4a90bc58398824f5a5c
Zakk Fast <zakkfastpro@gmail.com>
2021-10-04 17:37:36 -0700

    route to register user to mongo complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 // @route    POST api/users
 // @decs     Register user
@@ -16,12 +20,49 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() }); // Send bad request if error with json storin errors
     }
-    res.send("User Route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email }); //find user by email
+
+      if (user) {
+        //if there is already a user with that name send 400
+        res.status(400).json()({ errors: [{ msg: "User already exists!" }] });
+      }
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200", // size
+        r: "pg", //  rating
+        d: "mm", //   default if none found
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Set salt
+      const salt = await bcrypt.genSalt(10);
+      // Encrypt password
+      user.password = await bcrypt.hash(password, salt);
+      // Save user to db
+      await user.save();
+
+      // Return jsonwebtoken
+      res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
