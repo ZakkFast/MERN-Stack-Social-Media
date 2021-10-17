71115b3de9afd59e918d3b0d434dda9acacdaaf9
Zakk Fast <zakkfastpro@gmail.com>
2021-10-17 13:38:25 -0700

    set up registar and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Components
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
+import { Login } from './components/auth/Login';
+import { Register } from './components/auth/Register';
 
 import './App.css';
 
@@ -11,6 +14,12 @@ const App = () => (
     <Fragment>
       <Navbar />
       <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </Fragment>
   </Router>
 );
