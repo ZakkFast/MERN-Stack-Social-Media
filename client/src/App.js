657f875e38be282da9cc272beaf59fa4c11ed123
Zakk Fast <zakkfastpro@gmail.com>
2021-10-18 19:34:50 -0700

    set up local storage for auth for register user

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store.js';
+import { loadUser } from './actions/auth.js';
+import setAuthToken from './utils/setAuthToken.js';
 
 // Components
 import { Navbar } from './components/layout/Navbar';
@@ -14,7 +16,16 @@ import Alert from './components/layout/Alert'
 
 import './App.css';
 
-const App = () => (
+if(localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
+const App = () => { 
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, []);
+
+  return(
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -30,6 +41,6 @@ const App = () => (
       </Fragment>
     </Router>
   </Provider>
-);
+)};
 
 export default App;
