06043781deed99ea0c8724844500e1e202a51e76
Zakk Fast <zakkfastpro@gmail.com>
2021-10-18 18:14:38 -0700

    created Alert component

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 import { Login } from './components/auth/Login';
 import Register from './components/auth/Register.js';
+import Alert from './components/layout/Alert'
 
 import './App.css';
 
@@ -20,6 +21,7 @@ const App = () => (
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
