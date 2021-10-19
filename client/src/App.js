471233b8ad9735d2e0873ce99a685df36131236d
Zakk Fast <zakkfastpro@gmail.com>
2021-10-18 17:00:01 -0700

    set up alert reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store.js';
+
 // Components
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
@@ -10,18 +14,20 @@ import { Register } from './components/auth/Register';
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
---
+++ client/src/actions/types.js
@@ -0,0 +1,2 @@
+export const SET_ALERT = 'SET _ALERT'
+export const REMOVE_ALERT = 'REMOVE _ALERT'
